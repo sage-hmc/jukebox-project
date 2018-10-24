245b7fc42b801bcaa235ff1aba700710ddadb770
ksasakipz <ksasaki@students.pitzer.edu>
2018-10-24 15:50:54 -0700

    Added project juke with all added dependencies in it

---
+++ juke/SpotifyiOS.framework/SPTLoginSampleAppSwift-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  SPTLoginSampleAppSwift-Bridging-Header.h
+//  
+//
+//  Created by Kevin Sasaki on 10/24/18.
+//
+
+#ifndef SPTLoginSampleAppSwift_Bridging_Header_h
+#define SPTLoginSampleAppSwift_Bridging_Header_h
+
+
+#endif /* SPTLoginSampleAppSwift_Bridging_Header_h */
---
+++ juke/juke/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  juke
+//
+//  Created by Kevin Sasaki on 10/17/18.
+//  Copyright © 2018 Kevin Sasaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
